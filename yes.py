32aff4921b5433f5e61de321269fd651b1ceba0b
MelissaOzcan <34689581+MelissaOzcan@users.noreply.github.com>
2017-12-31 14:27:40 -0500

    Update yes.py

--- yes.py
+++ yes.py
@@ -1,16 +1,17 @@
 # Midyear project
 import random
-
 suspects = ("Tanish", "Chris", "Melissa", "Mr. Isecke", "Suspect 5", "Suspect 6")
-rooms = ("The Compsci Room", "Lower Caf", "Upper Caf", "The gym", "The grill", "The breezeway", "Mr. Zhang's room", "Mr Demeter's room", "Ms. Fillebrown's room")
+rooms = ("The Compsci Room", "Lower Caf", "Upper Caf", "The gym", "The grill", "The breezeway", "Mr. Zhang's room", "Mr. Demeter's room", "Ms. Fillebrown's room")
 
 
 # do we need a class? i dont see how we would use it
 def print_list():
+    # this part is from battle
     var3 = 0
     for c in suspects:
         print("%d : %s" % (var3, c))
         var3 += 1
+
 
 def clues():
     """
@@ -24,22 +25,35 @@ def clues():
     if suspect == "Chris":
         clues = ()
     if suspect == "Melissa":
-        clues = ("a cat hair", "")
+        clues = ()
     if suspect == "Mr. Isecke":
-        clues = ("rubber duck", "bagel", "glasses")
+        clues = ()
+
+
+def room_choice():
+    # so we pick three rooms for the clues to be hidden in
+    room_choice1 = random.choice(rooms)
+    room_choice2 = random.choice(rooms)
+    if room_choice1 == room_choice2:
+        room_choice2 = random.choice(rooms)
+    room_choice3 = random.choice(rooms)
+    if room_choice3 == room_choice1 or room_choice3 == room_choice2:
+        room_choice3 = random.choice(rooms)
+
 
 def main():
-    room_choice = random.choice(rooms)
     print("Welcome to Clue! In this game, you'll have to help Mr. Respass figure out who took his computer."
           "\nA list of suspects has already been made. But first of all, who are you? "
           "\nTo select a character, type in its number.")
-    print_list()
 
+    print_list()
+    # this next part is also from battle, its just error checking
     char_choice = int(input())
     while char_choice < 0 or char_choice >= len(suspects):
         choice = int(input("Please choose between 0 and " + str(len(suspects)) + "."))
 
-    print("Hello, %s. Think you can solve the mystery of who took Mr. Respass' computer? I sure hope you can." % char_choice)
+    # ok so this part doesn't work because it gives you the number of the character chosen, not the name
+    print("Hello, %s. Think you can solve the mystery of who took Mr. Respass' computer?" % char_choice)
 
     """ 
     alrighty now heres whats going to happen after this:
@@ -50,7 +64,6 @@ def main():
     5. ask if they know who did it for sure. if they get it right, they win. if not, they lose and game over.
     then u just sorta repeat all that till the game is over.
     """
-
 
 
 main()
