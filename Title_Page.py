168350eea0144b7bc0c63073e1c7f11e6aac2fc5
tanmeh21@begren.org <Hariom100>
2018-01-08 13:24:37 -0500

    Chris

--- Title_Page.py
+++ Title_Page.py
@@ -1,4 +1,4 @@
 from tkinter import *
 class Application(Frame):
     def create_widgets(self):
-        Label(text = "Welcome to this fucking game!")
+        Label(text = "Welcome to this fucking game!!")
