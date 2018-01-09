8dd1ba89b061c05325a1d1139e2192d9818d215c
ChristopherAttilano <chratt21@bergen.org>
2018-01-09 10:57:02 -0500

    qewrhtyui

--- Title_Page.py
+++ Title_Page.py
@@ -5,9 +5,9 @@ class Application(Frame):
     def __init__(self, master):
         super(Application,self).__init__(master)
         self.grid()
-        self.create_widgets()
+        self.create_widgets_page_one()
 
-    def create_widgets(self):
+    def create_widgets_page_one(self):
         self.label = Label(text="Welcome to  Chutes and Ladders!")
         self.label.grid(row = 2, column = 2)
 
@@ -18,11 +18,10 @@ class Application(Frame):
     def page_two(self):
         self.label["text"]= "IT WORKED"
         self.next_bttn.destroy()
-        imageSmall = PhotoImage(file="Picture.gif")
+        imageSmall = PhotoImage(file="Board.gif")
         w = Label(self, image=imageSmall)
         w.photo = imageSmall
         w.grid(row=5, column=1)
-
 
 root = Tk()
 app = Application(root)
