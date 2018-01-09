b79675566733e26ba4c044ec565cd91e0fae341e
ozcan <melozc21@bergen.org>
2018-01-09 11:01:51 -0500

    Merge branch 'master' of C:\Users\ozcan\PycharmProjects\CHris with conflicts.

--- Title_Page.py
+++ Title_Page.py
@@ -5,23 +5,19 @@ class Application(Frame):
     def __init__(self, master):
         super(Application,self).__init__(master)
         self.grid()
-        self.create_widgets_page_one()
+        self.create_widgets()
 
-    def create_widgets_page_one(self):
+    def page_one(self):
         self.label = Label(text="Welcome to  Chutes and Ladders!")
         self.label.grid(row = 2, column = 2)
 
         self.next_bttn = Button(self, text="Next", command=self.page_two)
         self.next_bttn.grid(row=1, column=1)
 
-
     def page_two(self):
         self.label["text"]= "IT WORKED"
         self.next_bttn.destroy()
-        imageSmall = PhotoImage(file="Board.gif")
-        w = Label(self, image=imageSmall)
-        w.photo = imageSmall
-        w.grid(row=5, column=1)
+
 
 root = Tk()
 app = Application(root)
