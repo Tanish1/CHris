492d2ba6a1532b6d97b91c192b910be66da8f35c
ChristopherAttilano <chratt21@bergen.org>
2018-01-18 13:12:35 -0500

    qewrhtyui

--- Title_Page.py
+++ Title_Page.py
@@ -9,7 +9,8 @@ class Application(Frame):
         self.grid()
         self.create_widgets_page_one()
         self.rolled_number = Label(self, text="")
-        self.var = 0
+        self.var1 = 0
+        self.var2 = 0
 
     def create_widgets_page_one(self):
         self.label = Label(text="Welcome to  Chutes and Ladders!")
@@ -22,7 +23,6 @@ class Application(Frame):
         self.next_bttn.destroy()
         self.label.destroy()
         self.roll_button = Button(self, text = "Roll",command = self.dice_roll_number)
-
         self.roll_button.grid(row=7, column = 4)
         imageSmall = PhotoImage(file="Board.jpg")
         w = Label(self, image=imageSmall)
@@ -31,79 +31,86 @@ class Application(Frame):
 
         self.rolled_number = Label(self, text = "")
         self.rolled_number.grid(row = 8, column = 4)
-        self.
+
+        self.player1_space = Label(self, text = "")
+        self.player1_space.grid(row = 9, column = 4)
+
+        self.winner = Label (self, text = "")
+        self.winner.grid(row = 10, column = 4)
 
     def dice_roll_number(self):
         dice_roll = random.randint(1,6)
-        self.var += dice_roll
+        self.var1 += dice_roll
         self.move_player()
-        print(self.player_pos)
+        print(self.player1_pos)
         self.rolled_number["text"] = "You rolled a " + str(dice_roll)
-        self.rolled_number["text"] = "You're at space " + str(self.var)
+        self.player1_space["text"] = "You're at space " + str(self.var1)
         imageSmall = PhotoImage(file="Photos/Dice" + str(dice_roll) +".gif")
         w = Label(self, image=imageSmall)
         w.photo = imageSmall
-        w.grid(row=8, column=2)
+        w.grid(row=8, column=2, rowspan = 4)
+        if self.var1 >= 100 or self.var2 >= 100:
+            self.winner["text"] = "Congrats you won!"
 
     def move_player(self):
         self.board = []
         for i in range(100):
             self.board.append(cell())
-        self.player_pos = self.var
-        if self.player_pos == 1:
-            self.player_pos = 38
-            self.var = self.player_pos
-        if self.player_pos == 4:
-            self.player_pos = 14
-            self.var = self.player_pos
-        if self.player_pos == 9:
-            self.player_pos = 31
-            self.var = self.player_pos
-        if self.player_pos == 16:
-            self.player_pos = 6
-            self.var = self.player_pos
-        if self.player_pos == 21:
-            self.player_pos = 42
-            self.var = self.player_pos
-        if self.player_pos == 36:
-            self.player_pos = 44
-            self.var = self.player_pos
-        if self.player_pos == 47:
-            self.player_pos = 26
-            self.var = self.player_pos
-        if self.player_pos == 49:
-            self.player_pos = 11
-            self.var = self.player_pos
-        if self.player_pos == 51:
-            self.player_pos = 67
-            self.var = self.player_pos
-        if self.player_pos == 56:
-            self.player_pos = 53
-            self.var = self.player_pos
-        if self.player_pos == 62:
-            self.player_pos = 19
-            self.var = self.player_pos
-        if self.player_pos == 64:
-            self.player_pos = 60
-            self.var = self.player_pos
-        if self.player_pos == 71:
-            self.player_pos = 91
-            self.var = self.player_pos
-        if self.player_pos == 80:
-            self.player_pos = 100
-            self.var = self.player_pos
-        if self.player_pos == 87:
-            self.player_pos = 24
-            self.var = self.player_pos
-        if self.player_pos == 93:
-            self.player_pos = 73
-            self.var = self.player_pos
-        if self.player_pos == 95:
-            self.player_pos = 75
-            self.var = self.player_pos
-        if self.player_pos == 98:
-            self.player_pos = 78
-            self.var = self.player_pos
+        self.player1_pos = self.var1
+        if self.player1_pos == 1:
+            self.player1_pos = 38
+            self.var1 = self.player1_pos
+        if self.player1_pos == 4:
+            self.player1_pos = 14
+            self.var1 = self.player1_pos
+        if self.player1_pos == 9:
+            self.player1_pos = 31
+            self.var1 = self.player1_pos
+        if self.player1_pos == 16:
+            self.player1_pos = 6
+            self.var1 = self.player1_pos
+        if self.player1_pos == 21:
+            self.player1_pos = 42
+            self.var1 = self.player1_pos
+        if self.player1_pos == 36:
+            self.player1_pos = 44
+            self.var1 = self.player1_pos
+        if self.player1_pos == 47:
+            self.player1_pos = 26
+            self.var1 = self.player1_pos
+        if self.player1_pos == 49:
+            self.player1_pos = 11
+            self.var1 = self.player1_pos
+        if self.player1_pos == 51:
+            self.player1_pos = 67
+            self.var1 = self.player1_pos
+        if self.player1_pos == 56:
+            self.player1_pos = 53
+            self.var1 = self.player1_pos
+        if self.player1_pos == 62:
+            self.player1_pos = 19
+            self.var1 = self.player1_pos
+        if self.player1_pos == 64:
+            self.player1_pos = 60
+            self.var1 = self.player1_pos
+        if self.player1_pos == 71:
+            self.player1_pos = 91
+            self.var1 = self.player1_pos
+        if self.player1_pos == 80:
+            self.player1_pos = 100
+            self.var1 = self.player1_pos
+        if self.player1_pos == 87:
+            self.player1_pos = 24
+            self.var1 = self.player1_pos
+        if self.player1_pos == 93:
+            self.player1_pos = 73
+            self.var1 = self.player1_pos
+        if self.player1_pos == 95:
+            self.player1_pos = 75
+            self.var1 = self.player1_pos
+        if self.player1_pos == 98:
+            self.player1_pos = 78
+            self.var1 = self.player1_pos
 
 class cell(object):
     def __init__(self):
